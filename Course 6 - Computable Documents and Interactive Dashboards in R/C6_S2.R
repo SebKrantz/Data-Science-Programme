ee4ed1c46355ef9937cadf88aadb016190acc598
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2021-06-25 08:40:56 +0300

    Adding S2 File.

---
+++ Course 6 - Computable Documents and Interactive Dashboards in R/C6_S2.R
@@ -0,0 +1,38 @@
+#############################################################
+# *************** R BASED DATA SCIENCE TRAINING *************
+# Course 6: Computable Documents and Interactive Dashboards 
+# -----------------------------------------------------------
+# Prepared by: Sebastian Krantz, ODI Fellow, MEPD, MoFPED
+# Permanent Contact: sebastian.krantz@graduateinstitute.ch
+#############################################################
+
+setwd("Course 6 - Computable Documents and Interactive Dashboards in R")
+
+# (7) Interactive Dashboards with 'flexdashboard' --------------------------
+#***************************************************************************
+
+# Dashboards are a useful way to communicate large amounts of information visually and quickly. 
+# Create one with the flexdashboard::flex_dashboard output format
+
+install.packages('flexdashboard')
+
+# Links: 
+# https://pkgs.rstudio.com/flexdashboard/
+# http://rmarkdown.rstudio.com/flexdashboard/
+# https://bookdown.org/yihui/rmarkdown/dashboards.html
+
+
+# (8) Case study: Make your own COVID-19 Dashboard -------------------------
+#***************************************************************************
+
+# Task: Make a COVID dahboard for Uganda, East Africa or the whole world, including vaccination progress. 
+# Use interactiv vidualizations provided by https://www.htmlwidgets.org/ (in particular plotly)
+
+# COVID Dashboard Example (using plotly for visualization): 
+# https://github.com/RamiKrispin/coronavirus_dashboard
+
+# Worldwide Data (Including Vaccination Progress)
+install.packages("COVID19")
+
+
+
